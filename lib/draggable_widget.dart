479b75d1046ca8e1af317f01e260b8a15ff7b9f8
İbrahim SEZER <ibrahimszr16@gmail.com>
2023-10-11 16:42:35 +0300

    Text with Draggable widget

--- lib/draggable_widget.dart
+++ lib/draggable_widget.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 double globalPosX = 0;
@@ -11,8 +10,8 @@ Key globalKey = const Key("");
 
 double containerWidth = 150; // Başlangıç genişliği
 double containerHeight = 150; // Başlangıç yüksekliği
-double containerLeft = 50; // X pozisyonu
-double containerTop = 50; // Y pozisyonu
+double containerLeft = 0; // X pozisyonu
+double containerTop = 0; // Y pozisyonu
 bool isResizing = false;
 Offset startPosition = Offset.zero;
 
@@ -37,7 +36,7 @@ class _PageDraggableState extends State<PageDraggable> {
         Positioned(
             top: 0,
             child: Container(
-                height: areaH * 0.2, width: areaW, color: Colors.red)),
+                height: areaH * 0.1, width: areaW, color: Colors.red)),
         ...widgets,
 
         ///Create Widget Button
@@ -48,67 +47,83 @@ class _PageDraggableState extends State<PageDraggable> {
 
         ///Delete Button
         deleteWidget,
+        resizeableWidget(context)
+
         ///Resizeable Widget
-        resizeableWidget
       ]),
     );
   }
 
-  Positioned get resizeableWidget {
+  Positioned resizeableWidget(BuildContext context) {
+    double areaW = MediaQuery.sizeOf(context).width;
+    double areaH = MediaQuery.sizeOf(context).height;
+    String resizeableText = "";
     return Positioned(
-        top: globalPosY,
-        right: globalPosX,
-        child: GestureDetector(
-          onPanStart: (details) {
-            if (details.localPosition.dx >= containerWidth - 20 &&
-                details.localPosition.dy >= containerHeight - 20) {
-              setState(() {
-                isResizing = true;
-                startPosition = details.localPosition;
-              });
-            }
-          },
-          onPanUpdate: (details) {
-            if (isResizing) {
-              setState(() {
-                if (containerWidth >= 50 && containerHeight >= 50) {
-                  double dx = details.localPosition.dx - startPosition.dx;
-                  double dy = details.localPosition.dy - startPosition.dy;
-                  containerWidth += dx;
-                  containerHeight += dy;
-                  startPosition = details.localPosition;
-                } else {
-                  containerHeight += 51;
-                  containerWidth += 51;
-                }
-              });
-            }
-          },
-          onPanEnd: (details) {
+      top: 150,
+      right: 150,
+      child: GestureDetector(
+        onPanStart: (details) {
+          if (details.localPosition.dx >= containerWidth - 20 &&
+              details.localPosition.dy >= containerHeight - 20) {
             setState(() {
-              isResizing = false;
+              resizeableText = "onPanStart !!!";
+              isResizing = true;
+              startPosition = details.localPosition;
             });
-          },
-          child: Container(
-            width: containerWidth,
-            height: containerHeight,
-            margin: EdgeInsets.only(left: containerLeft, top: containerTop),
-            decoration: BoxDecoration(
-              border: Border.all(width: 2, color: Colors.black),
-            ),
-            child: const Center(
-              child: Text(
-                "İçerik",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 15,
-                  decoration: TextDecoration.none,
-                ),
-              ),
+          }
+        },
+        onPanUpdate: (details) {
+          if (isResizing) {
+            setState(() {
+              if ((containerWidth >= 50 && containerHeight >= 50) &&
+                  (containerWidth <= (areaW - 10) &&
+                      containerHeight <= (areaH - 10)) &&
+                  (startPosition.dx <= areaW && startPosition.dx >= 0) &&
+                  (startPosition.dy <= areaH && startPosition.dy >= 0)) {
+                resizeableText = "Update!!";
+                double dx = details.localPosition.dx - startPosition.dx;
+                double dy = details.localPosition.dy - startPosition.dy;
+                containerWidth += dx;
+                containerHeight += dy;
+                startPosition = details.localPosition;
+              } else if (containerHeight <= 50) {
+                containerHeight = 51;
+              } else if (containerWidth <= 50) {
+                containerWidth = 50;
+              } else {
+                containerHeight = 51;
+                containerWidth = 51;
+              }
+            });
+          }
+        },
+        onPanEnd: (details) {
+          setState(() {
+            resizeableText = "Enddd !!";
+            isResizing = false;
+          });
+        },
+        child: Container(
+          width: containerWidth,
+          height: containerHeight,
+          margin: EdgeInsets.only(left: containerLeft, top: containerTop),
+          decoration: BoxDecoration(
+            border: Border.all(width: 2, color: Colors.black),
+          ),
+          child: const Center(
+            child: Text(
+              textWidthBasis: TextWidthBasis.parent,
+              textScaleFactor: 1,
+              "Deneme123123123123123Deneme",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  decoration: TextDecoration.none),
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 
   Positioned get deleteWidget {
@@ -162,10 +177,15 @@ class _PageDraggableState extends State<PageDraggable> {
                               onPressed: () {
                                 setState(() {
                                   Navigator.of(context).pop();
-                                  widgets.add(DraggableWidget2(
+                                  widgets.add(ReDragText(
                                     getText: myController.text,
                                   ));
                                   myController.text = "";
+
+                                  // widgets.add(DraggableWidget2(
+                                  //   getText: myController.text,
+                                  // ));
+                                  // myController.text = "";
                                 });
                               },
                               child: const Row(
@@ -225,33 +245,146 @@ class DraggableWidget2 extends StatefulWidget {
 class _DraggableWidget2State extends State<DraggableWidget2> {
   @override
   Widget build(BuildContext context) {
+    late double localTextW;
+    late double localTextH;
     return Positioned(
-      top: globalPosY + 100,
-      left: globalPosX + 100,
+      top: globalPosY,
+      left: globalPosX,
+      child: Draggable(
+          data: "myDatatext",
+          onDragUpdate: (details) {
+            setState(() {
+              posX = details.globalPosition.dx - (localTextW / 2);
+              posY = details.globalPosition.dy - (localTextH / 2);
+
+              globalPosX = posX;
+              globalPosY = posY;
+            });
+          },
+          feedback: const SizedBox(),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              double textBorderW = 100;
+              double textBorderH = 100;
+              localTextH = textBorderH;
+              localTextW = textBorderW;
+              return Container(
+                width: textBorderW,
+                height: textBorderH,
+                decoration: BoxDecoration(border: Border.all(width: 1)),
+                child: Text(
+                  textWidthBasis: TextWidthBasis.parent,
+                  textScaleFactor: 1,
+                  widget.getText,
+                  style: const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      decoration: TextDecoration.none),
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class ReDragText extends StatefulWidget {
+  const ReDragText({super.key, required this.getText});
+
+  final String getText;
+
+  @override
+  State<ReDragText> createState() => _ReDragTextState();
+}
+
+class _ReDragTextState extends State<ReDragText> {
+  @override
+  Widget build(BuildContext context) {
+    late double localTextW;
+    late double localTextH;
+    double areaW = MediaQuery.sizeOf(context).width;
+    double areaH = MediaQuery.sizeOf(context).height;
+    String resizeableText = "";
+
+    return Positioned(
+      top: globalPosY,
+      right: globalPosX,
       child: Draggable(
         data: "myDatatext",
         onDragUpdate: (details) {
-          setState(() {
-            posX = details.globalPosition.dx - DragContainer.sizeW / 2;
-            posY = details.globalPosition.dy - DragContainer.sizeH / 2;
+              setState(() {posX = details.globalPosition.dx - (localTextW*3.5);
+              posY = details.globalPosition.dy - (localTextH / 2);
 
-            globalPosX = posX;
-            globalPosY = posY;
-          });
-        },
-        feedback: Text(
-          widget.getText,
-          style: const TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              decoration: TextDecoration.none),
-        ),
-        child: Text(
-          widget.getText,
-          style: const TextStyle(
-              color: Colors.blue,
-              fontSize: 20,
-              decoration: TextDecoration.none),
+              globalPosX = -posX;
+              globalPosY = posY;
+
+              });
+            },
+        feedback: const SizedBox(),
+        child: GestureDetector(
+          onPanStart: (details) {
+              if (details.localPosition.dx >= containerWidth - 20 &&
+                  details.localPosition.dy >= containerHeight - 20) {
+                setState(() {
+                  resizeableText = "onPanStart !!!";
+                  isResizing = true;
+                  startPosition = details.localPosition;
+                });
+              }
+            },
+          onPanUpdate: (details) {
+              if (isResizing) {
+                setState(() {
+                  if ((containerWidth >= 50 && containerHeight >= 50) &&
+                      (containerWidth <= (areaW - 10) &&
+                          containerHeight <= (areaH - 10)) &&
+                      (startPosition.dx <= areaW && startPosition.dx >= 0) &&
+                      (startPosition.dy <= areaH && startPosition.dy >= 0)) {
+                    resizeableText = "Update!!";
+                    double dx = details.localPosition.dx - startPosition.dx;
+                    double dy = details.localPosition.dy - startPosition.dy;
+                    containerWidth += dx;
+                    containerHeight += dy;
+                    startPosition = details.localPosition;
+                  } else if (containerHeight <= 50) {
+                    containerHeight = 51;
+                  } else if (containerWidth <= 50) {
+                    containerWidth = 51;
+                  } else {
+                    containerHeight = 51;
+                    containerWidth = 51;
+                  }
+                });
+              }
+            },
+          onPanEnd: (details) {
+              setState(() {
+                resizeableText = "Enddd !!";
+                isResizing = false;
+              });
+            },
+          child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double textBorderW = 100;
+                  double textBorderH = 100;
+                  localTextH = textBorderH;
+                  localTextW = textBorderW;
+                  return Container(
+                    width: containerWidth,
+                    height: containerHeight,
+                    decoration: BoxDecoration(border: Border.all(width: 1)),
+                    child: Text(
+                      textWidthBasis: TextWidthBasis.parent,
+                      textScaleFactor: 1,
+                      widget.getText,
+                      style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                          decoration: TextDecoration.none),
+                    ),
+                  );
+                },
+              ),
         ),
       ),
     );
@@ -294,8 +427,7 @@ class _DraggableWidgetState extends State<DraggableWidget> {
       top: globalPosY,
       left: globalPosX,
       child: Draggable(
-        feedback: DragContainer(
-            brWidth: brWidth, brColor: brColor, textData: widget.txtData),
+        feedback: const SizedBox(),
         onDragStarted: () {},
         onDragUpdate: (details) {
           setState(() {
